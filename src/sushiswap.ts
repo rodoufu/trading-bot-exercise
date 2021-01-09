696f97d40b989f96ad0b4d0e85802a163a27288d
Rodolfo Araujo <rodoufu@gmail.com>
2021-01-09 14:22:09 -0300

    get price.

---
+++ src/sushiswap.ts
@@ -0,0 +1,13 @@
+import {ChainId, Token, WETH, Fetcher, Route, Price} from '@sushiswap/sdk'
+
+export async function getPrice(baseToken: Token, quoteToken: Token, webProvider?:any): Promise<Price> {
+	const pair = await Fetcher.fetchPairData(quoteToken, baseToken, webProvider);
+
+	const route = new Route([pair], baseToken);
+	return route.midPrice;
+}
+
+export {
+	Token,
+	ChainId,
+}
